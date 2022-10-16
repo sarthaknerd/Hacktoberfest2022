530381f8d635d971b2c7a8e3d00ac94c5fd0321b
SARTHAK RANJAN <56467031+sarthaknerd@users.noreply.github.com>
2022-10-17 01:14:10 +0530

    whatsapp Automation in Python

---
+++ Add Code Here/PYTHON/whatsappAutomation.py
@@ -0,0 +1,8 @@
+#Sarthaknerd
+#pip install pywhatkit before importing pywhatkit
+import pywhatkit
+ 
+ 
+pywhatkit.sendwhatmsg("+919xxxxxxxxx",
+                      "hacktober 2022",
+                      18, 30)
